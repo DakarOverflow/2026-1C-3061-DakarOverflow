f94dfc10fac5c0c73b459d24334b5f2186efd980
l4uder <l4uder@gmail.com>
2026-06-15 17:13:18 -0300

    patch: Patch FreeCamera.cs for multi platform support by abstracting DLL setup

--- TGC.MonoGame.TP/Program.cs
+++ TGC.MonoGame.TP/Program.cs
@@ -5,17 +5,6 @@ namespace TGC.MonoGame.TP
 {
     public static class Program
     {
-        
-        [DllImport("/home/pc/RiderProjects/2026-1C-3061-DakarOverflow/TGC.MonoGame.TP/bin/Debug/net8.0/runtimes/linux-x64/native/libSDL2-2.0.so.0", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int SDL_SetHint(string name, string value);
-        [DllImport("/home/pc/RiderProjects/2026-1C-3061-DakarOverflow/TGC.MonoGame.TP/bin/Debug/net8.0/runtimes/linux-x64/native/libSDL2-2.0.so.0", CallingConvention = CallingConvention.Cdecl)]
-        private static extern IntPtr SDL_GetError();
-        [DllImport("/home/pc/RiderProjects/2026-1C-3061-DakarOverflow/TGC.MonoGame.TP/bin/Debug/net8.0/runtimes/linux-x64/native/libSDL2-2.0.so.0", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int SDL_SetRelativeMouseMode(int enabled);
-    
-        [DllImport("/home/pc/RiderProjects/2026-1C-3061-DakarOverflow/TGC.MonoGame.TP/bin/Debug/net8.0/runtimes/linux-x64/native/libSDL2-2.0.so.0", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int SDL_GetRelativeMouseMode();
-        
         [STAThread]
         static void Main()
         {
