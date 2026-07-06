7798db186f5af31a725eb9e62875a80a2552869d
FabiVGroellF <fgroellfrontado@frba.utn.edu.ar>
2026-07-06 13:58:32 -0300

    Eliminado del uso de BoudingSphere porque no se usa en el juego

--- TGC.MonoGame.TP/BoundingVolumeFactory.cs
+++ TGC.MonoGame.TP/BoundingVolumeFactory.cs
@@ -5,20 +5,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 public static class BoundingVolumeFactory
 {
-    public static BoundingSphere CreateWorldBoundingSphere(WorldObject obj)
-    {
-        BoundingSphere? mergedSphere = null;
 
-        foreach (ModelMesh mesh in obj.Model.Model.Meshes)
-        {
-            BoundingSphere worldSphere = mesh.BoundingSphere.Transform(obj.World);
-            mergedSphere = mergedSphere.HasValue
-                ? BoundingSphere.CreateMerged(mergedSphere.Value, worldSphere)
-                : worldSphere;
-        }
-
-        return mergedSphere ?? new BoundingSphere(obj.World.Translation, 0f);
-    }
 
     public static BoundingBox CreateWorldBoundingBox(WorldObject obj)
     {
